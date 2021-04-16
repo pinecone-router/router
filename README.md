2094aae9667b584779b196c8b6c3f0f426d55f6a
Rafik El Hadi Houari <rehhouari@gmail.com>
2021-04-16 04:43:51 +0100

    update

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-# Alpine Plugin Template
+# Alpine Router
 
-One short line about the plugin.
+A simple router for Alpine.js. (WIP)
 
-![GitHub tag (latest by date)](https://img.shields.io/github/v/tag/[repo]?label=version&style=flat-square)
-![GitHub file size in bytes](https://img.shields.io/github/size/[repo]/dist/[file].js?label=min%20%28no%20gzip%29&style=flat-square)
-[![Monthly downloads via CDN](https://data.jsdelivr.com/v1/package/gh/[repo]/badge)](https://www.jsdelivr.com/package/gh/[repo])
+![GitHub tag (latest by date)](https://img.shields.io/github/v/tag/rehhouari/alpine-router?label=version&style=flat-square)
+![GitHub file size in bytes](https://img.shields.io/github/size/rehhouari/alpine-router]/dist/alpine-router.js?label=min%20%28no%20gzip%29&style=flat-square)
+[![Monthly downloads via CDN](https://data.jsdelivr.com/v1/package/gh/[repo]/badge)](https://www.jsdelivr.com/package/gh/rehhouari/alpine-router)
 
 ## About
 
-A short bit of information about the plugin and what it does.
+A simple router for use with Alpine.js.
 
 ## Installation
 
@@ -16,11 +16,45 @@ A short bit of information about the plugin and what it does.
 
 Include the following `<script>` tag in the `<head>` of your document:
 
-``` html
-<script src="https://insert-cdn-link-here.com"></script>
+```html
+<script src="https://cdn.jsdeliver.com/gh/rehhouari/alpine-router/dist/alpine-router.umd.min.js"></script>
 ```
 
-> **Important**: This must be added **before** loading Alpine.js when using CDN links.
+## Usage
+
+Create an Alpine component with the `x-router` attribute
+
+The routers will be children with the `x-route` and `x-handler` attributes
+
+The `x-handler` must be a method of the router component.
+
+```html
+<div x-data="handle()" x-router>
+	<template x-route="/hello/:name" x-handler="hello"></template>
+	<template x-route="/" x-handler="main"></template>
+	<template x-route="notfound" x-handler="notfound"></template>
+</div>
+```
+
+The handler takes `props` as an argument which will have path variales.
+
+```js
+function handle() {
+	return {
+		main(props) {
+			console.log('main');
+		},
+		hello(props) {
+			console.log('hello,', props.name);
+		},
+		notfound(props) {
+			console.log('not found');
+		},
+	};
+}
+```
+
+> **Important**: This must be added **before** loading Alpine.js.
 
 ## Versioning
 
@@ -28,6 +62,6 @@ This projects follow the [Semantic Versioning](https://semver.org/) guidelines.
 
 ## License
 
-Copyright (c) 2020 [author] and contributors
+Copyright (c) 2020 Rafik El Hadi Houari and contributors
 
 Licensed under the MIT license, see [LICENSE.md](LICENSE.md) for details.
--- src/index.js
+++ src/index.js
@@ -1,17 +1,219 @@
-const AlpinePlugin = {
-    start() {
+const AlpineRouter = {
+	routes: [],
+	isLocation: !!(window.history.location || window.location),
+	notfound: function () {
+		console.log('Alpine Router: not found');
+	},
 
-    }
+	start() {
+		if (!window.Alpine) {
+			throw new Error('Alpine is require for `Alpine Router` to work.');
+		}
+
+		// Whenever a component is initialized, check if it is a router
+		// and run test the children if they're valid routes
+		Alpine.onComponentInitialized((component) => {
+			if (component.$el.hasAttribute('x-router')) {
+				Array.from(component.$el.children).forEach((el) => {
+					if (el.hasAttribute('x-route')) {
+						this.processRoute(el, component);
+					}
+				});
+			}
+		});
+
+		// Intercept click event in links
+		document.querySelectorAll('a').forEach((el) => {
+			// Taken from page.js https://github.com/visionmedia/page.js/blob/master/index.js#L370
+			// el.nodeName for svg links are 'a' instead of 'A'
+			while (el && 'A' !== el.nodeName.toUpperCase()) el = el.parentNode;
+			if (!el || 'A' !== el.nodeName.toUpperCase()) return;
+
+			// check if link is inside an svg
+			// in this case, both href and target are always inside an object
+			var svg =
+				typeof el.href === 'object' &&
+				el.href.constructor.name === 'SVGAnimatedString';
+
+			// Ignore if tag has
+			// 1. "download" attribute
+			// 2. rel="external" attribute
+			if (
+				el.hasAttribute('download') ||
+				el.getAttribute('rel') === 'external'
+			) {
+				return;
+			}
+
+			var link = el.getAttribute('href');
+
+			// Check for mailto: in the href
+			if (link && link.indexOf('mailto:') > -1) return;
+
+			// check target
+			// svg target is an object and its desired value is in .baseVal property
+			if (svg ? el.target.baseVal : el.target) return;
+
+			// x-origin
+			// note: svg links that are not relative don't call click events (and skip page.js)
+			// consequently, all svg links tested inside page.js are relative and in the same origin
+			if (!svg && !this.sameOrigin(el.href)) return;
+
+			el.addEventListener(
+				'click',
+				(e) => {
+					e.preventDefault();
+					this.navigate(e.target.getAttribute('href'));
+				},
+				false
+			);
+		});
+		
+		window.addEventListener('popstate', (e) => router.navigate(e.detail));
+	
+		// navigate to the current page to handle it
+		this.navigate(location.pathname + location.hash);
+	},
+
+	/**
+	 * Take the template element of a route and the router component
+	 * and test if it can be added or not
+	 */
+	processRoute(el, component) {
+		if (el.tagName.toLowerCase() !== 'template') {
+			throw new Error(
+				'Alpine Router: x-routes must be used on a template tag.'
+			);
+		}
+
+		if (el.hasAttribute('x-handler') == false) {
+			throw new Error(
+				'Alpine Router: x-route must have a handler (x-handler="handler")'
+			);
+		}
+
+		let path = el.getAttribute('x-route');
+		let handlerName = el.getAttribute('x-handler');
+		let handler = component.getUnobservedData()[handlerName];
+
+		if (path == 'notfound') {
+			// register the route as a 404 handler
+			this.notfound = handler;
+		} else {
+			// register the new route.
+			this.routes.push(new Route(path, handler));
+		}
+	},
+
+	/**
+	 * Match the path with specified routes
+	 * https://github.com/vijitail/simple-javascript-router/blob/master/src/router/Router.js#L14
+	 */
+	match(route, requestPath) {
+		let paramNames = [];
+		let regexPath =
+			route.path.replace(/([:*])(\w+)/g, (_full, _colon, name) => {
+				paramNames.push(name);
+				return '([^/]+)';
+			}) + '(?:/|$)';
+
+		let params = {};
+		let routeMatch = requestPath.match(new RegExp(regexPath));
+		if (routeMatch !== null) {
+			params = routeMatch.slice(1).reduce((params, value, index) => {
+				if (params === null) params = {};
+				params[paramNames[index]] = value;
+				return params;
+			}, null);
+		}
+
+		route.setProps(params);
+
+		return routeMatch;
+	},
+
+	/**
+	 * Go to the specified path without reloading
+	 * https://github.com/vijitail/simple-javascript-router/blob/master/src/router/Router.js#L37
+	 */
+	navigate(path) {
+		const route = this.routes.filter((route) => this.match(route, path))[0];
+		if (!route) this.notfound();
+		else {
+			history.pushState({}, '', path);
+			route.handle();
+		}
+	},
+
+	/**
+	 * Convert to a URL object
+	 * https://github.com/visionmedia/page.js/blob/4f9991658f9b9e3de9b6059bade93693af24d6bd/page.js#L888
+	 */
+	_toURL(href) {
+		if (typeof URL === 'function' && this.isLocation) {
+			return new URL(href, window.location.toString());
+		} else {
+			var anc = window.document.createElement('a');
+			anc.href = href;
+			return anc;
+		}
+	},
+
+	/**
+	 * Check if `href` is the same origin.
+	 * https://github.com/visionmedia/page.js/blob/4f9991658f9b9e3de9b6059bade93693af24d6bd/page.js#L888
+	 */
+	sameOrigin(href) {
+		if (!href || !this.isLocation) return false;
+
+		var url = this._toURL(href);
+		var loc = window.location;
+
+		/*
+		   When the port is the default http port 80 for http, or 443 for
+		   https, internet explorer 11 returns an empty string for loc.port,
+		   so we need to compare loc.port with an empty string if url.port
+		   is the default port 80 or 443.
+		   Also the comparition with `port` is changed from `===` to `==` because
+		   `port` can be a string sometimes. This only applies to ie11.
+		*/
+		return (
+			loc.protocol === url.protocol &&
+			loc.hostname === url.hostname &&
+			(loc.port === url.port ||
+				(loc.port === '' && (url.port == 80 || url.port == 443)))
+		); // jshint ignore:line
+	},
+	_samePath(url) {
+		if (!this.isLocation) return false;
+		var loc = window.location;
+		return url.pathname === loc.pathname && url.search === loc.search;
+	},
+};
+
+class Route {
+	constructor(path, handler) {
+		this.path = path;
+		this.handler = handler;
+	}
+
+	setProps(newProps) {
+		this.props = newProps;
+	}
+
+	handle() {
+		return this.handler(this.props);
+	}
 }
 
-const alpine = window.deferLoadingAlpine || ((callback) => callback())
+const alpine = window.deferLoadingAlpine || ((callback) => callback());
 
-window.AlpinePlugin = AlpinePlugin
+window.AlpineRouter = AlpineRouter;
 
 window.deferLoadingAlpine = function (callback) {
-    window.AlpinePlugin.start()
+	window.AlpineRouter.start();
 
-    alpine(callback)
-}
+	alpine(callback);
+};
 
-export default AlpinePlugin
+export default AlpineRouter;
